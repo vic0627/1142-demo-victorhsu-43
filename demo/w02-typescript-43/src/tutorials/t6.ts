1f21b06097bf555fee02a171afbe7c97b29803de
victor_xu <pure90719@gmail.com>
2026-03-07 15:52:03 +0800

    W02-P4: Modules - import and export in t6.ts and t6a.ts

---
+++ demo/w02-typescript-43/src/tutorials/t6.ts
@@ -0,0 +1,6 @@
+import studentNew, { sayHello, person, type Student } from "./t6a";
+
+sayHello("TypeScript");
+// person = 'victor'; // Error: Cannot assign to 'person' because it is a constant.
+console.log("person_43", person);
+console.log("studentNew_43", studentNew);
